add9f625ef0838fd27bb078b87a7a00f458572c0
Jose <jjsancherzr.pex@sanitas.es>
2019-08-16 00:43:01 +0200

    Se crean los Outlets y Actions de los botones

--- iOS-Calculator/Controller/Home/HomeViewController.swift
+++ iOS-Calculator/Controller/Home/HomeViewController.swift
@@ -10,6 +10,32 @@ import UIKit
 
 final class HomeViewController: UIViewController {
 
+    // MARK: - Outlets
+    //Result
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    // Numbers
+    @IBOutlet weak var number0: UIButton!
+    @IBOutlet weak var number1: UIButton!
+    @IBOutlet weak var number2: UIButton!
+    @IBOutlet weak var number3: UIButton!
+    @IBOutlet weak var number4: UIButton!
+    @IBOutlet weak var number5: UIButton!
+    @IBOutlet weak var number6: UIButton!
+    @IBOutlet weak var number7: UIButton!
+    @IBOutlet weak var number8: UIButton!
+    @IBOutlet weak var number9: UIButton!
+    
+    // Operators
+    @IBOutlet weak var operatorAC: UIButton!
+    @IBOutlet weak var operatorPlusMinus: UIButton!
+    @IBOutlet weak var OperatorPercent: UIButton!
+    @IBOutlet weak var operatorResult: UIButton!
+    @IBOutlet weak var operatorAddition: UIButton!
+    @IBOutlet weak var operatorSubstraction: UIButton!
+    @IBOutlet weak var operatorMultiplication: UIButton!
+    @IBOutlet weak var operatorDivision: UIButton!
+    
     // MARK: - Initialization
     init() {
         super.init(nibName: nil, bundle: nil)
@@ -22,6 +48,32 @@ final class HomeViewController: UIViewController {
     // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
     }
+    
+    // MARK: - Button Actions
+    //Numbers
+    @IBAction func numberAction(_ sender: UIButton) {
+        print(sender.tag)
+    }
+    
+    //Operators
+    @IBAction func operatorACAction(_ sender: Any) {
+    }
+    @IBAction func operatorPlusMinusAction(_ sender: Any) {
+    }
+    @IBAction func operatorPercentAction(_ sender: Any) {
+    }
+    @IBAction func operatorResultAction(_ sender: Any) {
+    }
+    @IBAction func operatorAdditionAction(_ sender: Any) {
+    }
+    @IBAction func operatorSubstractionAction(_ sender: Any) {
+    }
+    @IBAction func operatorMultiplicationAction(_ sender: Any) {
+    }
+    @IBAction func operatorDivisionAction(_ sender: Any) {
+    }
+    @IBAction func operatorDecimalAction(_ sender: Any) {
+    }
+    
 }
