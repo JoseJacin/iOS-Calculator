d7ffe5ac96ad5f94c4fd1cb5fdc53fc12aa0b1a4
Jose <jjsancherzr.pex@sanitas.es>
2019-08-15 23:25:40 +0200

    Se crea la pantalla Home
    Se crea todo lo necesario para la pantalla Home

--- iOS-Calculator/AppDelegate.swift
+++ iOS-Calculator/AppDelegate.swift
@@ -15,7 +15,24 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        // Setup
+        setupView()
+        
         return true
+    }
+    
+    // MARK: - Private Methods
+    // Inicializa la primera vista de la app
+    private func setupView() {
+        // Se instancia la Vista completa de la app
+        window = UIWindow(frame: UIScreen.main.bounds)
+        // Se instancia el ViewController de la vista que se quire mostrar
+        let vc = HomeViewController()
+        // Se asigna al Controlador de Vista Principal de window (Vista completa de la app) el ViewController de la vista
+        window?.rootViewController = vc
+        // Indicamos que se inicie y que se muestre
+        window?.makeKeyAndVisible()
     }
 }
 
